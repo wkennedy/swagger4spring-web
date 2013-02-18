b13170aab6abe946222482510a5a8383fa093871
Will Kennedy <will.kennedy@sparcedge.com>
2013-02-18 13:16:50 -0500

    Added JavaDoc

--- src/main/java/com/sparc/swagger4springweb/util/AnnotationUtils.java
+++ src/main/java/com/sparc/swagger4springweb/util/AnnotationUtils.java
@@ -15,6 +15,10 @@ import java.util.List;
 
 public class AnnotationUtils {
 
+    /**
+     * @param method Method - check to see if this method has a parameter annotated with @ApiParam
+     * @return boolean true if this method has a parameter annotated with @ApiParam
+     */
     public static boolean hasApiParam(Method method) {
         Annotation[][] parameterAnnotations = method.getParameterAnnotations();
         for (Annotation[] parameterAnnotation : parameterAnnotations) {
@@ -28,6 +32,10 @@ public class AnnotationUtils {
         return false;
     }
 
+    /**
+     * @param annotations Annotation[] - check to see if this array contains an annotation of type ApiParam
+     * @return boolean true if this array contains an annotation of type ApiParam
+     */
     public static boolean hasApiParam(Annotation[] annotations) {
         for (Annotation annotation : annotations) {
             if (annotation instanceof ApiParam) {
@@ -37,17 +45,29 @@ public class AnnotationUtils {
         return false;
     }
 
+    /**
+     * @param method Method - get the request mapping for this method
+     * @return String - the value of the RequestMapping annotation on this method
+     */
     public static String getMethodRequestMappingValue(Method method) {
         RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
         String requestMappingValue = "/";
-        String[] requestMappingValues = requestMapping.value();
-        if (requestMappingValues != null && requestMappingValues.length > 0) {
-            requestMappingValue = requestMappingValues[0];
+        if (requestMapping != null) {
+            String[] requestMappingValues = requestMapping.value();
+            if (requestMappingValues != null && requestMappingValues.length > 0) {
+                requestMappingValue = requestMappingValues[0];
+            }
         }
 
         return requestMappingValue;
     }
 
+    /**
+     * @param method Method
+     * @return List<AnnotatedParameter> - if the method contains parameters annotated with ApiParam, RequestMapping,
+     * PathVariable, or RequestBody, this method will return a list of AnnotatedParameter objects based on the values
+     * of the parameter annotations
+     */
     public static List<AnnotatedParameter> getAnnotatedParameters(Method method) {
         List<AnnotatedParameter> annotatedParameters = new ArrayList<AnnotatedParameter>();
         Paranamer paranamer = new BytecodeReadingParanamer();
@@ -70,8 +90,8 @@ public class AnnotationUtils {
                     break;
                 } else if (!hasApiParam(annotations) &&
                         (annotation instanceof RequestMapping ||
-                        annotation instanceof PathVariable ||
-                        annotation instanceof RequestBody)) {
+                                annotation instanceof PathVariable ||
+                                annotation instanceof RequestBody)) {
                     AnnotatedParameter annotatedParameter = new AnnotatedParameter();
                     annotatedParameter.setAnnotation(annotation);
                     annotatedParameter.setParameterType(parameterType);
