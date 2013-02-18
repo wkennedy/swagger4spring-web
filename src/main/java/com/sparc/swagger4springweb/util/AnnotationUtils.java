820974e30f0b8b16d045645eca00e677ce639bb5
Will Kennedy <will.kennedy@sparcedge.com>
2013-02-18 11:22:52 -0500

    Added RequestBody annotation to parameter schema mapping

--- src/main/java/com/sparc/swagger4springweb/util/AnnotationUtils.java
+++ src/main/java/com/sparc/swagger4springweb/util/AnnotationUtils.java
@@ -5,6 +5,7 @@ import com.thoughtworks.paranamer.BytecodeReadingParanamer;
 import com.thoughtworks.paranamer.Paranamer;
 import com.wordnik.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.lang.annotation.Annotation;
@@ -67,7 +68,10 @@ public class AnnotationUtils {
                     annotatedParameter.setParameterName(parameterName);
                     annotatedParameters.add(annotatedParameter);
                     break;
-                } else if (!hasApiParam(annotations) && (annotation instanceof RequestMapping || annotation instanceof PathVariable)) {
+                } else if (!hasApiParam(annotations) &&
+                        (annotation instanceof RequestMapping ||
+                        annotation instanceof PathVariable ||
+                        annotation instanceof RequestBody)) {
                     AnnotatedParameter annotatedParameter = new AnnotatedParameter();
                     annotatedParameter.setAnnotation(annotation);
                     annotatedParameter.setParameterType(parameterType);
